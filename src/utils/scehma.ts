ea203f50971c4990e5acb444f3bb0b720462f995
Dam <60846068+j2h30728@users.noreply.github.com>
2024-10-09 22:33:58 +0900

    feat: 검색 페이지 및 검색 기능 추가

--- src/utils/scehma.ts
+++ src/utils/scehma.ts
@@ -6,3 +6,10 @@ export const responseSchema = z
   })
   .trim()
   .max(200, "Response should be less then 200 characters long.");
+
+export const keywordSchema = z
+  .string({
+    required_error: "Search-keyword is required.",
+  })
+  .trim()
+  .max(20, "Search-keyword should be less then 20 characters long.");
