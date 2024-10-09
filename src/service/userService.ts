4de1f3278eba1663c52ec353fa9754eb89e17461
Dam <60846068+j2h30728@users.noreply.github.com>
2024-10-09 22:32:49 +0900

    feat: tab bar 수정-검색 및 마이페이지 항목 추가

--- src/service/userService.ts
+++ src/service/userService.ts
@@ -1,6 +1,24 @@
 "use server";
 
 import db from "@/utils/db";
+import { notFound } from "next/navigation";
+
+import { getSession } from "@/utils/session";
+
+export const getUserInfoBySession = async () => {
+  const session = await getSession();
+  if (session.id) {
+    const user = await db.user.findUnique({
+      where: {
+        id: session.id,
+      },
+    });
+    if (user) {
+      return user;
+    }
+  }
+  notFound();
+};
 
 export const isEmailExist = async (email: string): Promise<boolean> => {
   const user = await db.user.findUnique({
